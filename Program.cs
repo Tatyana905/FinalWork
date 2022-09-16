ad6aaef8320ac87887014590a5469acad8280bc8
Tatyana905 <905478@mail.ru>
2022-09-16 16:43:16 +0300

    добавлена программа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите данные через пробел");
+string[] Array = Console.ReadLine().Split(' ').ToArray();
+int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3) count++;
+}
+string[] result = new string[count];
+for (int i = 0; i < result.Length; i++)
+{
+    for (int j = 0; j < Array.Length; j++)
+    {
+        if (Array[j].Length <= 3 && Array[j] != string.Empty)
+        {
+            result[i] = Array[j];
+            Array[j] = string.Empty;
+            break;
+        }
+    }
+    Console.Write($"{result[i]} ");
+}
